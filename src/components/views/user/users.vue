996f8793a3e8964ab9778ca21ff0879c6cbb6e5d
tangli <tangli2@umxwe.com>
2021-02-04 18:15:29 +0800

    udpate

--- src/components/views/user/users.vue
+++ src/components/views/user/users.vue
@@ -3,24 +3,40 @@
         <div class="crumbs">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>
-                    <i class="el-icon-lx-cascades"></i> 基础表格
+                    <i class="el-icon-lx-cascades"></i> 会员列表
                 </el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="container">
             <div class="handle-box">
-                <el-button
-                    type="primary"
-                    icon="el-icon-delete"
-                    class="handle-del mr10"
-                    @click="delAllSelection"
-                >批量删除</el-button>
-                <el-select v-model="query.address" placeholder="地址" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
-                </el-select>
-                <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
-                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+                <el-form :inline="true" :model="formInline" class="demo-form-inline">
+                  <el-form-item label="用户名称">
+                    <el-input placeholder="请输入用户名称"></el-input>
+                  </el-form-item>
+                  <el-form-item label="手机号码">
+                    <el-input placeholder="请输入手机号码"></el-input>
+                  </el-form-item>
+                  <el-form-item label="注册时间">
+                    <el-date-picker
+                      type="daterange"
+                      range-separator="至"
+                      start-placeholder="开始日期"
+                      end-placeholder="结束日期">
+                    </el-date-picker>
+                  </el-form-item>
+                  <el-form-item label="状态">
+                    <el-select placeholder="所有状态" class="handle-select mr10" clearble>
+                        <el-option key="1" label="真人" value="真人"></el-option>
+                        <el-option key="2" label="假人" value="假人"></el-option>
+                    </el-select>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button type="primary" @click="handleSearch">搜索</el-button>
+                    <el-button>重置</el-button>
+                    <el-button>导出</el-button>
+                  </el-form-item>
+                </el-form>
+
             </div>
             <el-table
                 :data="tableData"
@@ -30,13 +46,24 @@
                 header-cell-class-name="table-header"
                 @selection-change="handleSelectionChange"
             >
-                <el-table-column type="selection" width="55" align="center"></el-table-column>
-                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+                <!-- <el-table-column type="selection" width="55" align="center"></el-table-column> -->
+                <el-table-column prop="id" label="ID" width="55"></el-table-column>
+                <el-table-column prop="account" label="账号"></el-table-column>
                 <el-table-column prop="name" label="用户名"></el-table-column>
-                <el-table-column label="账户余额">
-                    <template slot-scope="scope">￥{{scope.row.money}}</template>
+                <el-table-column prop="num" label="人数"></el-table-column>
+                <el-table-column prop="num" label="活跃人数"></el-table-column>
+                <el-table-column prop="num" label="账户余额">
+                    <template slot-scope="scope">{{scope.row.money}}</template>
                 </el-table-column>
-                <el-table-column label="头像(查看大图)" align="center">
+                <el-table-column prop="num" label="冻结金额"></el-table-column>
+                <el-table-column prop="num" label="利息宝"></el-table-column>
+                <el-table-column prop="name" label="上级用户"></el-table-column>
+                <el-table-column prop="name" label="邀请码"></el-table-column>
+                <el-table-column prop="" label="线上充值"></el-table-column>
+                <el-table-column prop="" label="线上提现"></el-table-column>
+                <el-table-column prop="date" label="注册时间"></el-table-column>
+                <el-table-column prop="IP" label="最后登录IP"></el-table-column>
+                <!-- <el-table-column label="头像(查看大图)" align="center">
                     <template slot-scope="scope">
                         <el-image
                             class="table-td-thumb"
@@ -44,17 +71,16 @@
                             :preview-src-list="[scope.row.thumb]"
                         ></el-image>
                     </template>
-                </el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
-                <el-table-column label="状态" align="center">
+                </el-table-column> -->
+                <!-- <el-table-column prop="address" label="地址"></el-table-column> -->
+                <el-table-column label="状态" align="center" width="120">
                     <template slot-scope="scope">
-                        <el-tag
-                            :type="scope.row.state==='成功'?'success':(scope.row.state==='失败'?'danger':'')"
-                        >{{scope.row.state}}</el-tag>
+                        <el-tag :type="success">真人</el-tag>
+                        <el-tag :type="danger">代理</el-tag>
                     </template>
                 </el-table-column>
 
-                <el-table-column prop="date" label="注册时间"></el-table-column>
+                <!-- <el-table-column prop="date" label="注册时间"></el-table-column> -->
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
                         <el-button
@@ -113,7 +139,18 @@ export default {
                 pageIndex: 1,
                 pageSize: 10
             },
-            tableData: [],
+            tableData: [{
+                id:"fdjsk",
+                account:'1256525452',
+                name:"fdjsk",
+                num:23215,
+                money:"fdjsk",
+                thumb:"fdjsk",
+                address:"fdjsk",
+                state:"fdjsk",
+                time:"2021-10-12 20:21:00",
+                IP:"192.168.1.125"
+            }],
             multipleSelection: [],
             delList: [],
             editVisible: false,
@@ -155,16 +192,16 @@ export default {
         handleSelectionChange(val) {
             this.multipleSelection = val;
         },
-        delAllSelection() {
-            const length = this.multipleSelection.length;
-            let str = '';
-            this.delList = this.delList.concat(this.multipleSelection);
-            for (let i = 0; i < length; i++) {
-                str += this.multipleSelection[i].name + ' ';
-            }
-            this.$message.error(`删除了${str}`);
-            this.multipleSelection = [];
-        },
+        // delAllSelection() {
+        //     const length = this.multipleSelection.length;
+        //     let str = '';
+        //     this.delList = this.delList.concat(this.multipleSelection);
+        //     for (let i = 0; i < length; i++) {
+        //         str += this.multipleSelection[i].name + ' ';
+        //     }
+        //     this.$message.error(`删除了${str}`);
+        //     this.multipleSelection = [];
+        // },
         // 编辑操作
         handleEdit(index, row) {
             this.idx = index;
