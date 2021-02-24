d3359e2ac6d722ba706c8dc3e7740f4c32097e6d
tangli <tangli2@umxwe.com>
2021-02-24 21:52:24 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     productionSourceMap: false,
     devServer: {
         host: '0.0.0.0',
-        port: '8083',
+        port: '8400',
         proxy: {
             '/api':{
                 //target:'http://192.168.1.169:9009/',
